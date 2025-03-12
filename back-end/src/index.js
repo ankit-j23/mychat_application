7961d641c78ebeb92f44569504937160cedb4fa9
Ankit <ank558899@gmail.com>
2025-03-12 08:33:24 +0530

    some pakages updated

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -4,12 +4,17 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.use("/api/auth" , authRoutes);
 app.use("/api/message" , messageRoutes)
